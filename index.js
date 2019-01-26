5f00064e2bf2df5f8d98b6a730fbe6bffd663705
victorsiret <victorsiret@gmail.com>
2019-01-26 15:10:21 +0100

    content + accordions

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
-/* Default state of sidebar (1=open) */
+// Default state of sidebar (1=open)
 let sideBarOut = 1
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// Detect Screen Size
+let device
+if (window.innerWidth < 500) {device = 2;}
+else if (window.innerWidth < 1050) {device = 1;}
+else {device = 0;}
+
+// Set the width of the sidebar to 250px and the left margin of the page content to 250px
 function openNav() {
+      // Open/Close for Desktop
+      if (device === 0) {
         if (sideBarOut === 1) {
-        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("mySidebar").style.left = "-300px";
+        document.getElementById("mySidebar").style.opacity = "0.5"; 
         document.getElementById("main").style.marginLeft = "0";
         sideBarOut = 0;
         }
-    else {
-        document.getElementById("mySidebar").style.width = "300px";
+        else {
+        document.getElementById("mySidebar").style.left = "0px";
+        document.getElementById("mySidebar").style.opacity = "1"; 
         document.getElementById("main").style.marginLeft = "300px";
         sideBarOut = 1;
         }
+      }
+      // Open/Close for Tablet
+      else if (device === 1) {
+        if (sideBarOut === 1) {
+        document.getElementById("mySidebar").style.left = "-300px";
+        document.getElementById("mySidebar").style.opacity = "0.5"; 
+        document.getElementById("main").style.marginLeft = "0";
+        sideBarOut = 0;
+        }
+        else {
+        document.getElementById("mySidebar").style.left = "0px";
+        document.getElementById("mySidebar").style.opacity = "1"; 
+        document.getElementById("main").style.marginLeft = "300px";
+        sideBarOut = 1;
+        }
+      }
+      // Open/Close for Phone
+      else {
+        if (sideBarOut === 1) {
+        document.getElementById("mySidebar").style.left = "-100vw";
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("mySidebar").style.opacity = "0.5"; 
+        sideBarOut = 0;
+        }
+        else {
+        document.getElementById("mySidebar").style.left = "0px";
+        document.getElementById("mySidebar").style.width = "100vw";
+        document.getElementById("mySidebar").style.opacity = "1"; 
+        sideBarOut = 1;
+        }
+      }
     }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  // Set the width of the sidebar to 0 and the left margin of the page content to 0
   function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    // If Desktop/Laptop
+    if (device === 0) {
+      document.getElementById("mySidebar").style.left = "-300px";
+      document.getElementById("mySidebar").style.opacity = "0.5"; 
+      document.getElementById("main").style.marginLeft = "0";
+      sideBarOut = 0;
+    }
+    // If Tablet
+    else if (device === 1) {
+    document.getElementById("mySidebar").style.left = "-300px";
+    document.getElementById("mySidebar").style.opacity = "0.5"; 
     document.getElementById("main").style.marginLeft = "0";
     sideBarOut = 0;
+    }
+    // If Phone
+    else {
+    document.getElementById("mySidebar").style.left = "-100vw";
+    document.getElementById("mySidebar").style.opacity = "0.5"; 
+    document.getElementById("main").style.marginLeft = "0";
+    sideBarOut = 0;
+    }
   }
