53c6d1f74754ad43ab9cbecff25692eb8e00534f
victorsiret <38690840+victorsiret@users.noreply.github.com>
2019-05-09 13:49:48 +0200

    icons and various fixes

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 // Default state of sidebar (1=open)
-let sideBarOut = 1
+let sideBarOut = 0
 
 // Detect Screen Size
 let device
-if (window.innerWidth < 500) {device = 2;}
-else if (window.innerWidth < 1050) {device = 1;}
+if (window.screen.width < 500) {device = 1;}
 else {device = 0;}
+
+if (device === 0) {
+  document.getElementById("mySidebar").style.left = "0px";
+  document.getElementById("mySidebar").style.opacity = "1"; 
+  document.getElementById("main").style.marginLeft = "300px";
+  sideBarOut = 1;
+}
 
 // Set the width of the sidebar to 250px and the left margin of the page content to 250px
 function openNav() {
-      // Open/Close for Desktop
+      // Open/Close for Desktop/Laptop/Tablet
       if (device === 0) {
-        if (sideBarOut === 1) {
-        document.getElementById("mySidebar").style.left = "-300px";
-        document.getElementById("mySidebar").style.opacity = "0.5"; 
-        document.getElementById("main").style.marginLeft = "0";
-        sideBarOut = 0;
-        }
-        else {
-        document.getElementById("mySidebar").style.left = "0px";
-        document.getElementById("mySidebar").style.opacity = "1"; 
-        document.getElementById("main").style.marginLeft = "300px";
-        sideBarOut = 1;
-        }
-      }
-      // Open/Close for Tablet
-      else if (device === 1) {
         if (sideBarOut === 1) {
         document.getElementById("mySidebar").style.left = "-300px";
         document.getElementById("mySidebar").style.opacity = "0.5"; 
@@ -58,19 +49,12 @@ function openNav() {
   
   // Set the width of the sidebar to 0 and the left margin of the page content to 0
   function closeNav() {
-    // If Desktop/Laptop
+    // If Desktop/Laptop/Tablet
     if (device === 0) {
       document.getElementById("mySidebar").style.left = "-300px";
       document.getElementById("mySidebar").style.opacity = "0.5"; 
       document.getElementById("main").style.marginLeft = "0";
       sideBarOut = 0;
-    }
-    // If Tablet
-    else if (device === 1) {
-    document.getElementById("mySidebar").style.left = "-300px";
-    document.getElementById("mySidebar").style.opacity = "0.5"; 
-    document.getElementById("main").style.marginLeft = "0";
-    sideBarOut = 0;
     }
     // If Phone
     else {
